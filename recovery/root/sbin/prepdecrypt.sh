f546b51eccb3bcc02022101eb653e38cceb15a11
mauronofrio matarrese <poronofrio@gmail.com>
2019-02-25 19:36:21 +0100

    Removing Keymaster 3 support

--- recovery/root/sbin/prepdecrypt.sh
+++ recovery/root/sbin/prepdecrypt.sh
@@ -9,7 +9,6 @@ relink()
 
 relink /sbin/qseecomd
 relink /sbin/android.hardware.gatekeeper@1.0-service-qti
-relink /sbin/android.hardware.keymaster@3.0-service-qti
 relink /sbin/android.hardware.keymaster@4.0-service-qti
 
 /sbin/setprop crypto.ready 1
