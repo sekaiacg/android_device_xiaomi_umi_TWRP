c78d1479041db504907b83af69742a5115894c45
mauronofrio matarrese <poronofrio@gmail.com>
2019-03-18 02:03:03 +0100

    Updating variables for metadata decryption

--- recovery/root/sbin/prepdecrypt.sh
+++ recovery/root/sbin/prepdecrypt.sh
@@ -2,13 +2,15 @@
 
 relink()
 {
-	/sbin/cp $1 ${1}_old
-	sed 's|/system/bin/linker64|///////sbin/linker64|' "${1}_old" > "$1"
-	chmod 755 $1
+	fname=$(basename "$1")
+	target="/sbin/$fname"
+	sed 's|/system/bin/linker64|///////sbin/linker64|' "$1" > "$target"
+	chmod 755 $target
 }
 
 relink /sbin/qseecomd
 relink /sbin/android.hardware.gatekeeper@1.0-service-qti
 relink /sbin/android.hardware.keymaster@4.0-service-qti
 
-/sbin/setprop crypto.ready 1
+setprop prep.decrypt 1
+exit 0
