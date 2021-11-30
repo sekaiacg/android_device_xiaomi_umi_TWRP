ebae1517d4de2a7a18df3b905546adbf1148551e
sekaiacg <sekaiacg@gmail.com>
2021-11-30 18:42:42 +0800

    device: Improve compatibility
    Support vendorsetup.sh
    OrangeFox simplified Chinese translation is zh_CH.xml
    
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,7 +149,9 @@ TW_USE_TOOLBOX := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
+ifeq ($(TW_DEFAULT_LANGUAGE),)
 TW_DEFAULT_LANGUAGE := zh_CN
+endif
 TW_DEFAULT_BRIGHTNESS := 300
 ifneq ($(PRODUCT_RELEASE_NAME),lmi)
 TW_Y_OFFSET := 88
