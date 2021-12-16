4aa70c92834149d8e5e448773d29a3697212fddb
sekaiacg <sekaiacg@gmail.com>
2021-12-16 12:57:41 +0800

    device: Add missing libandroidicu
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- device.mk
+++ device.mk
@@ -47,4 +47,6 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 # PRODUCT_RELEASE_NAME ro.twrp.device.name
 PRODUCT_PROPERTY_OVERRIDES += ro.twrp.device.name=$(PRODUCT_RELEASE_NAME)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/prebuilt/$(PRODUCT_RELEASE_NAME)/modules,recovery/root/vendor/lib/modules)
+PRODUCT_COPY_FILES += \
+    $(OUT_DIR)/target/product/umi/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/prebuilt/$(PRODUCT_RELEASE_NAME)/modules,recovery/root/vendor/lib/modules)
